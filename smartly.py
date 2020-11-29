525a6a1dc3d49b2a9df6b62a912981108d0e4e4f
Haikal Khairul <haikalaqimi@gmail.com>
2020-11-29 04:04:01 +0000

    Error handling

--- smartly.py
+++ smartly.py
@@ -28,12 +28,12 @@ def get_word_and_definitions(text):
     word_N_def = []
 
     for i in important_words:
-    	print(i)
-    	word_N_def.append([i, gd.get_definition(i)])
+    	if(len(gd.get_definition(i)) != 0):
+    		word_N_def.append([i, gd.get_definition(i)])
 
     return word_N_def
 
 
 # text = "Neurogenesis is the process by which new neurons are formed in the brain. Neurogenesis is crucial when an embryo is developing, but also continues in certain brain regions after birth and throughout our lifespan. The mature brain has many specialised areas of function, and neurons that differ in structure and connections. The hippocampus, for example, which is a brain region that plays an important role in memory and spatial navigation, alone has at least 27 different types of neurons. The incredible diversity of neurons in the brain results from regulated neurogenesis during embryonic development. During the process, neural stem cells differentiate—that is, they become any one of a number of specialised cell types—at specific times and regions in the brain."
-text = 'The radial nerve, which is from the fifth cervical spinal nerve to the first thoracic spinal nerve, originates as the continuation of the posterior cord of the brachial plexus. This nerve enters the lower triangular space (an imaginary space bounded by, amongst others, the shaft of the humerus and the triceps brachii) of the arm and lies deep to the triceps brachii. Here it travels with the deep artery of the arm, which sits in the radial groove of the humerus. This fact is very important clinically as a fracture of the shaft of the bone here can cause lesions or even transections in the nerve.'
-print((get_word_and_definitions(text)))
+# text = 'The radial nerve, which is from the fifth cervical spinal nerve to the first thoracic spinal nerve, originates as the continuation of the posterior cord of the brachial plexus. This nerve enters the lower triangular space (an imaginary space bounded by, amongst others, the shaft of the humerus and the triceps brachii) of the arm and lies deep to the triceps brachii. Here it travels with the deep artery of the arm, which sits in the radial groove of the humerus. This fact is very important clinically as a fracture of the shaft of the bone here can cause lesions or even transections in the nerve.'
+# print((get_word_and_definitions(text)))
