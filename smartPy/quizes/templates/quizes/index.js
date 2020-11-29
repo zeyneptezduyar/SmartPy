396a97b7b4475aa4fdb5fef8e67fe7854ee7d308
Finn Hudson <11finnh@gmail.com>
2020-11-29 00:24:13 +0000

    added nice shit

---
+++ smartPy/quizes/templates/quizes/index.js
@@ -0,0 +1,3 @@
+let words = ["afforest", "blithesome", "capricious", "chivalrous"]
+let definitions = ["establish a forest on previously unforested land", "carefree and happy and lighthearted", 
+    "determined by chance or impulse rather than by necessity", ]
